6ef12cc252179f49bc2c1d9abada2d4e874bc57c
kyoheiu <thiasos.o@gmail.com>
2021-03-08 06:43:45 +0900

    Add themantic break, setext header, and blockquote-block

--- src/mrk.nim
+++ src/mrk.nim
@@ -11,6 +11,7 @@ type
     header4,
     header5,
     header6,
+    themanticbreak,
     blockquote,
     unorderedlist,
     orderedlist,
@@ -23,7 +24,7 @@ type
     softbreak,
     link,
     em,
-    lineong,
+    strong,
     code,
     image,
     text
@@ -54,28 +55,43 @@ type
     children: seq[Block]
 
 let
-  reHeader = re"^(#|##|###|####|#####|######) "
-  reBlockquote = re"^> "
-  reBulletListDashSpace = re"^- "
-  reBulletListPlusSpace = re"^\+ "
-  reBulletListAsteSpace = re"^\* "
-  reBulletListDashPare = re"^-\)"
-  reBulletListPlusPare = re"^\+\)"
-  reBulletListAstePare = re"^\*\)"
-  reOrderedListSpaceStart = re"1\. "
-  reOrderedListPareStart = re"1\)"
-  reOrderedListSpace = re"^(2|3|4|5|6|7|8|9)\. "
-  reOrderedListPare = re"^(2|3|4|5|6|7|8|9)\)"
-  reCodeBlock = re"^(```|~~~)"
+  reThematicBreak = re"^(| |  |   )(\*{3,}|-{3,}|_{3,})"
+  reSetextHeader1 = re"^(| |  |   )(=+)"
+  reSetextHeader2 = re"^(| |  |   )(--+)"
+  reAtxHeader = re"^(| |  |   )(#|##|###|####|#####|######) "
+  reBlockquote = re"^(| |  |   )>(| )"
+  reBulletListDashSpace = re"^(| |  |   )- "
+  reBulletListPlusSpace = re"^(| |  |   )\+ "
+  reBulletListAsteSpace = re"^(| |  |   )\* "
+  reBulletListDashPare = re"^(| |  |   )-\)"
+  reBulletListPlusPare = re"^(| |  |   )\+\)"
+  reBulletListAstePare = re"^(| |  |   )\*\)"
+  reOrderedListSpaceStart = re"^(| |  |   )1\. "
+  reOrderedListPareStart = re"^(| |  |   )1\)"
+  reOrderedListSpace = re"^(| |  |   )(2|3|4|5|6|7|8|9)\. "
+  reOrderedListPare = re"^(| |  |   )(2|3|4|5|6|7|8|9)\)"
+  reCodeBlock = re"^(| |  |   )(```|~~~)"
+  reParagraph = re"^(| |  |   )[^\*-_=+#>123456789(```)(~~~)]"
 
-proc isHeader(line: string): bool =
-  match(line, reHeader)
+proc isSetextHeader1(line: string): bool =
+  match(line, reSetextHeader1)
+proc isSetextHeader2(line: string): bool =
+  match(line, reSetextHeader2)
+
+proc isThemanticBreak(line: string): bool =
+  match(line, reThematicBreak)
+
+proc isAtxHeader(line: string): bool =
+  match(line, reAtxHeader)
 
 proc isBlockquote(line: string): bool =
   match(line, reBlockquote)
 
 proc isCodeFence(line: string): bool =
   match(line, reCodeBlock)
+
+proc isParagraph(line: string): bool =
+  match(line, reParagraph)
 
 proc isBulletListDashSpace(line: string): bool =
   match(line, reBulletListDashSpace)
@@ -115,22 +131,22 @@ proc newToggle(): ToggleContainer =
 proc parseHeader(line: string): Block =
   case line.splitWhitespace[0]:
     of "#":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header1, values: Inline(kind: text, value: @[str]))
     of "##":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header2, values: Inline(kind: text, value: @[str]))
     of "###":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header3, values: Inline(kind: text, value: @[str]))
     of "####":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header4, values: Inline(kind: text, value: @[str]))
     of "#####":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header5, values: Inline(kind: text, value: @[str]))
     of "######":
-      let str = line.replace(reHeader)
+      let str = line.replace(reAtxHeader)
       return Block(kind: header6, values: Inline(kind: text, value: @[str]))
 
 proc parseBlockquote(line: string): Block =
@@ -143,11 +159,22 @@ proc parseParagraph(line: string): Block =
 proc parseLine(s: string): seq[Block] =
   var mdast: seq[Block]
   var lineBlock: string
+  var blockquoteSeq: seq[string]
   var unorderedListSeq: seq[string]
   var orderedListSeq: seq[string]
   var container = newToggle()
 
   for line in s.splitLines:
+
+    block blockquotes:
+      if container.toggleBlockquote:
+        if not (line.isParagraph or line.isBlockquote):
+          mdast.add(Block(kind: blockquote, values: Inline(kind: text, value: blockquoteSeq)))
+          blockquoteSeq = @[]
+          container.toggleBlockquote = false
+          break blockquotes
+        else:
+          blockquoteSeq.add(line)
 
     block bulletListDashSpace:
       if container.toggleBulletListDashSpace:
@@ -179,28 +206,12 @@ proc parseLine(s: string): seq[Block] =
         lineblock = ""
         container.toggleCodeBlock = false
 
-    elif line.isEmptyOrWhitespace:
-      if not lineBlock.isEmptyOrWhitespace:
-        mdast.add(parseParagraph(lineBlock))
-        lineBlock = ""
-
-    elif line.isCodeFence:
-      if lineBlock != "":
-        mdast.add(parseParagraph(lineBlock))
-        lineBlock = ""
-      container.toggleCodeBlock = true
-
-    elif line.isHeader:
-      if lineBlock != "":
-        mdast.add(parseParagraph(lineBlock))
-      mdast.add(parseHeader(line))
-      lineBlock = ""
-    
     elif line.isBlockquote:
       if lineBlock != "":
         mdast.add(parseParagraph(lineBlock))
-      mdast.add(parseBlockquote(line))
-      lineBlock = ""
+        lineBlock = ""
+      blockquoteSeq.add(line.replace(reBlockquote))
+      container.toggleBlockquote = true
     
     elif line.isBulletListDashSpace:
       if lineBlock != "":
@@ -253,6 +264,43 @@ proc parseLine(s: string): seq[Block] =
         lineBlock = ""
       orderedListSeq.add(line.replace(reOrderedListPareStart))
       container.toggleOrderedListPare = true
+
+    elif line.isCodeFence:
+      if lineBlock != "":
+        mdast.add(parseParagraph(lineBlock))
+        lineBlock = ""
+      container.toggleCodeBlock = true
+    
+    elif line.isAtxHeader:
+      if lineBlock != "":
+        mdast.add(parseParagraph(lineBlock))
+      mdast.add(parseHeader(line))
+      lineBlock = ""
+    
+    elif line.isSetextHeader1:
+      if lineBlock != "":
+        mdast.add(Block(kind: header1, values: Inline(kind: text, value: @[lineBlock])))
+        lineBlock = ""
+      else:
+        lineBlock.add(line)
+    
+    elif line.isSetextHeader2:
+      if lineBlock != "":
+        mdast.add(Block(kind: header2, values: Inline(kind: text, value: @[lineBlock])))
+        lineBlock = ""
+      else:
+        mdast.add(Block(kind: themanticbreak, values: Inline()))
+    
+    elif line.isThemanticBreak:
+      if lineBlock != "":
+        mdast.add(parseParagraph(lineBlock))
+        lineBlock = ""
+      mdast.add(Block(kind: themanticbreak, values: Inline()))
+
+    elif line.isEmptyOrWhitespace:
+      if not lineBlock.isEmptyOrWhitespace:
+        mdast.add(parseParagraph(lineBlock))
+        lineBlock = ""
 
     else:
       lineBlock.add(line)
