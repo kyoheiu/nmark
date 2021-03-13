512a4991008bb0cd9d12668783071684a46b778c
kyoheiu <thiasos.o@gmail.com>
2021-03-13 14:04:10 +0900

    Fix unittest

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,4 +1,26 @@
-import unittest, mrk
+import unittest, strutils, sequtils, json
+import mrkpkg/def, mrkpkg/parseline, mrkpkg/parser
+
+proc testProc*(file: string): string =
+  var resultSeq: seq[Block]
+  var mdast: seq[Block]
+  var lineBlock: string
+  var unorderedListSeq: seq[string]
+  var orderedListSeq: seq[string]
+  var flag = newFlag()
+  var s = readFile(file)
+
+  for line in s.splitLines:
+    var str = line
+    parseLine(flag, lineBlock, mdast, resultSeq, str)
+  if lineBlock != "":
+    mdast.add(openParagraph(lineBlock))
+  resultSeq = concat(resultSeq, mdast)
+  echo pretty(%resultSeq)
+  var resultHtml: string
+  for mdast in resultSeq:
+    resultHtml.add(mdast.parseMdast)
+  return resultHtml
 
 test "test1":
   check testProc("testfiles/1.md") == """
@@ -10,4 +32,14 @@ test "test1":
 <p>This is a markdown-parser.</p>
 <h4>heading 4</h4>
 <p>Hello, World!</p>
+"""
+
+test "example32":
+  check testProc("testfiles/example32.md") == """
+<h1>foo</h1>
+<h2>foo</h2>
+<h3>foo</h3>
+<h4>foo</h4>
+<h5>foo</h5>
+<h6>foo</h6>
 """
