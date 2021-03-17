1f51efb8c3c93c4db0c192a57a0f4114c3e329f8
kyoheiu <thiasos.o@gmail.com>
2021-03-18 06:36:04 +0900

    Refactor: split proc

---
+++ src/mrkpkg/mdParser.nim
@@ -0,0 +1,47 @@
+import strutils, sequtils
+import def, mdToAst
+
+proc mdParser*(s: string): seq[Block] =
+  var flag = newFlag()
+  var lineBlock: string
+  var mdast: seq[Block]
+  var resultSeq: seq[Block]
+
+  for line in s.splitLines:
+    var str = line
+    mdToAst(flag, lineBlock, mdast, resultSeq, str)
+
+  if flag.flagBlockQuote:
+    if lineBlock != "":
+      mdast.add(openParagraph(lineBlock))
+    resultSeq.add(openContainerBlock(blockQuote, mdast))
+    mdast = @[]
+
+  elif flag.flagUnorderedList:
+    if lineBlock != "":
+      mdast.add(openList(lineBlock))
+    resultSeq.add(openContainerBlock(unOrderedList, mdast))
+    mdast = @[]
+
+  elif flag.flagOrderedList:
+    if lineBlock != "":
+      mdast.add(openList(lineBlock))
+    resultSeq.add(openContainerBlock(orderedList, mdast))
+    mdast = @[]
+  
+  elif lineBlock != "":
+    if flag.flagIndentedCodeBlock:
+      mdast.add(openCodeBlock(indentedCodeBlock, lineBlock))
+    else:
+      mdast.add(openParagraph(lineBlock))
+
+  resultSeq = concat(resultSeq, mdast)
+
+  return resultSeq
+
+  #var resultHtml: string
+
+  #for ast in resultSeq:
+    #resultHtml.add(ast.astToHtml)
+
+  #return resultHtml
