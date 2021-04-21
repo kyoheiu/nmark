9bece92dd17e638ff99a58b26603a3871dc701a6
Kyohei Uto <kyoheiu@outlook.com>
2021-04-21 09:37:04 +0900

    spec-test to 264

--- src/nmark.nim
+++ src/nmark.nim
@@ -64,8 +64,8 @@ when isMainModule:
   let
     f = parseFile("testfiles/spec-test.json")
   var
-    begins = 43
-    ends = 301
+    begins = 265
+    ends = 270
   for j in f:
     let
       j = f[begins-1]
