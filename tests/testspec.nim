81a5778e7c1ef3a8e5d3f4d2b14b1309f1200dc7
Kyohei Uto <kyoheiu@outlook.com>
2021-08-28 07:05:27 +0900

    Improve test flow

--- tests/testspec.nim
+++ tests/testspec.nim
@@ -1,29 +1,29 @@
-import unittest, json
+import unittest, strutils, json
 import ../src/nmark
 
-type SpecKind = enum
-  tabs
-  backslash
-  entity
-  thematicBreak
-  atxHeading
-  setextHeading
-  indentedCode
-  fencedCode
-  html
-  linkReference
-  paragraph
-  blockQuote
-  listItem
-  lists
-  code
-  emphasis
-  links
-  images
-  autolinks
-  rawHtml
-  otherInline
-  all
+#type SpecKind = enum
+  #tabs
+  #backslash
+  #entity
+  #thematicBreak
+  #atxHeading
+  #setextHeading
+  #indentedCode
+  #fencedCode
+  #html
+  #linkReference
+  #paragraph
+  #blockQuote
+  #listItem
+  #lists
+  #code
+  #emphasis
+  #links
+  #images
+  #autolinks
+  #rawHtml
+  #otherInline
+  #all
 
 proc specTest() =
   let
@@ -31,78 +31,92 @@ proc specTest() =
   var
     begins: int
     ends: int
-    sKind: SpecKind
-  
-  sKind = rawHtml
+#    sKind: SpecKind
 
-  case sKind
+  echo "Which specification do you want to test?"
+  echo """
+  1:tabs  2:backslash   3:entity
+  4:thematicBreak   5:atxHeading  6:setextHeading
+  7:indentedCode  8:fencedCode  9:html
+  10:linkReference  11:paragraph  12:blockQuote
+  13:listItem 14:lists  15:code
+  16:emphasis 17:links  18:images
+  19:autolinks  20:rawHtml  21:otherInline
+  22:all"""
 
-  of tabs:
+  var num = readLine(stdin).parseInt
+
+  case num
+
+  of 1:
     begins = 1
     ends = 12
-  of backslash:
+  of 2:
     begins = 12
     ends = 25
-  of entity:
+  of 3:
     begins = 25
     ends = 42
-  of thematicBreak:
+  of 4:
     begins = 42
     ends = 62
-  of atxHeading:
+  of 5:
     begins = 62
     ends = 80
-  of setextHeading:
+  of 6:
     begins = 80
     ends = 107
-  of indentedCode:
+  of 7:
     begins = 107
     ends = 119
-  of fencedCode:
+  of 8:
     begins = 119
     ends = 148
-  of html:
+  of 9:
     begins = 148
     ends = 192
-  of linkReference:
+  of 10:
     begins = 192
     ends = 219
-  of paragraph:
+  of 11:
     begins = 219
     ends = 228
-  of blockQuote:
+  of 12:
     begins = 228
     ends = 253
-  of listItem:
+  of 13:
     begins = 253
     ends = 301
-  of lists:
+  of 14:
     begins = 301
     ends = 327
-  of code:
+  of 15:
     begins = 327
     ends = 350
-  of emphasis:
+  of 16:
     begins = 350
     ends = 481
-  of links:
+  of 17:
     begins = 481
     ends = 571
-  of images:
+  of 18:
     begins = 571
     ends = 593
-  of autolinks:
+  of 19:
     begins = 593
     ends = 612
-  of rawHtml:
+  of 20:
     begins = 612
     ends = 633
-  of otherInline:
+  of 21:
     begins = 633
     ends = 653
-  of all:
+  of 22:
     begins = 1
     ends = 653
+  else:
+    echo "This is not right input."
+    return
 
   for j in f:
     let
